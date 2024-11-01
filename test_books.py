f18eed4e691bb9198e5bdfdda225ed9d4eba1838
narasyimmha <narasyimmha@gmail.com>
2024-11-01 12:24:00 +0000

    updated test file

--- test_books.py
+++ test_books.py
@@ -9,7 +9,7 @@ books = [book1,book2]
 
 @pytest.fixture
 def library_init():
-    library = Library()
+    library = Library(books)
 
 def test_init():
   
@@ -17,17 +17,14 @@ def test_init():
     assert book.author == author
     assert book.title == title
 
-def test_add_book(library_init):
-    library_init.add_book(books)
-    assert len(library_init.books) == 2
+def test_add_book():
+    library = Library()
+    assert len(library.books) == 2
 
 def test_return_books(library_init):
     
-    books = [book1,book2]
-    library_init.add_book(books)
     assert len(library_init.books) == 2
 
 def test_remove_books(library_init):
-    library_init.add_book(books)
     library_init.remove_book(books)
     assert len(library_init.books) == 0
