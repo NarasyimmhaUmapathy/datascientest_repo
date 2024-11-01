1f75b15d6180ff1f589571aa3be6f04b49e46ab2
narasyimmha <narasyimmha@gmail.com>
2024-11-01 12:27:03 +0000

    updated test file

--- test_books.py
+++ test_books.py
@@ -17,13 +17,14 @@ def test_init():
     assert book.author == author
     assert book.title == title
 
-def test_add_book():
-    library = Library()
-    assert len(library.books) == 2
+def test_add_book(library_init):
+    book3 = library_init.add_book(book3)
+    assert len(library_init.books) == 3
 
-def test_return_books(library_init):
+def test_return_books():
+    library = Library(books)
     
-    assert len(library_init.books) == 2
+    assert len(library.books) == 2
 
 def test_remove_books(library_init):
     library_init.remove_book(books)
