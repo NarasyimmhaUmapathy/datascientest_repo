4da494d4b6c3d9bd0cf64e6c012e7c94ff75b289
narasyimmha <narasyimmha@gmail.com>
2024-11-01 07:38:57 +0000

    adding wallet test files

---
+++ test_wallet.py
@@ -0,0 +1,20 @@
+from wallet import Wallet, InsufficientAmount
+import pytest
+
+def test_default_balance(Wallet):
+	wal = Wallet()
+	assert wal.initial_amount == 0
+
+def test_new_wallet(Wallet):
+	wak = Wallet(initial_amount=100)
+	assert wak.balance == 100
+
+def test_add_caash(Wallet):
+	wal = Wallet(inital_amount=10)
+	wal.add_cash(90)
+	assert wal.balance == 100
+
+def test_wallet_spend_cash_raises_exception_on_insufficient_amount():
+    wallet = Wallet()
+    with pytest.raises(InsufficientAmount):
+        wallet.spend_cash(100)
